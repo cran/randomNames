9e9d5b970fe6a62c538d8b3f69149104d4d3ca9f
Damian W. Betebenner <dbetebenner@nciea.org>
2019-03-07 15:50:03 +0000

    version 1.4-0.0

--- R/zzz.R
+++ R/zzz.R
@@ -6,6 +6,6 @@ function(libname, pkgname) {
 `.onAttach` <-
 function(libname, pkgname) {
 	if (interactive()) {
-		packageStartupMessage('randomNames ', paste(paste(unlist(strsplit(as.character(packageVersion("randomNames")), "[.]")), c(".", "-", ".", ""), sep=""), collapse=""),'  For help type: help("randomNames")')
+		packageStartupMessage(magenta$bold('randomNames',paste(paste0(unlist(strsplit(as.character(packageVersion("randomNames")), "[.]")), c(".", "-", ".", "")), collapse=""),' (3-7-2019). For help: >help("randomNames") or visit https://centerforassessment.github.io/randomNames'))
 	}
 }
